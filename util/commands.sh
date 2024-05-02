f08b8befa8a4ab68d7cdf72fe34b03d0da1e20a6
Travis Grammer <travisjgrammer@gmail.com>
2024-05-02 11:03:34 -0400

    add commands util; fix FAQ wording (#24)

--- util/commands.sh
+++ util/commands.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-curl -s -H "Authorization: Bot $BOT_TOKEN" https://discord.com/api/v10/applications/581956766246633475/commands | jq '[.[]|select(.type==1)| {"name","description","options"}]'
+# https://discord.com/developers/docs/interactions/application-commands#get-global-application-commands
+curl -s -H "Authorization: Bot ${GOLEM_API_TOKEN}" https://discord.com/api/v10/applications/${GOLEM_ID}/commands \
+| jq '.[] |= del(.id,.version,.nsfw,.application_id,.dm_permission)' \
+| yj -y
